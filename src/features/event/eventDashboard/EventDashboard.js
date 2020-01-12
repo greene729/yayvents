de757a0bf4edf3d82adf1342eb1707abb8765023
Alex Greene <alex@roofsnap.com>
2020-01-12 11:32:33 -0500

    Add nav bar and styling. Add event list, event list item, and event list attendee. Add EventForm

---
+++ src/features/event/eventDashboard/EventDashboard.js
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+import { Grid, Button } from "semantic-ui-react";
+import EventList from "../eventList/EventList";
+import EventForm from "../EventForm/EventForm";
+
+class EventDashboard extends Component {
+    state = {
+        // events: eventsFromDashboard,
+        isOpen: false
+    };
+
+    handleIsOpenToggle = () => {
+        this.setState(({isOpen}) => ({
+            isOpen: !isOpen
+        }));
+    };
+
+    render() {
+        const { events, isOpen } = this.state;
+        return (
+            <Grid>
+                <Grid.Column width={10}>
+                    <EventList events={events} />
+                </Grid.Column>
+                <Grid.Column width={6}>
+                    <Button
+                        onClick={this.handleIsOpenToggle}
+                        positive
+                        content='Create Event'
+                    />
+                    {isOpen && (
+                        <EventForm cancelFormOpen={this.handleIsOpenToggle} />
+                    )}
+                </Grid.Column>
+            </Grid>
+        );
+    }
+}
+
+export default EventDashboard;
